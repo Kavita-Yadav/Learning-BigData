69f5647f75aaed3a12c4bc3fb5efd607edd8c295
Kavita Yadav <53824088+Kavita-Yadav@users.noreply.github.com>
2019-08-07 15:32:03 +0800

    Update ExampleCountMovieRated.py

--- HDFS-and-MapReduce/ExampleCountMovieRated.py
+++ HDFS-and-MapReduce/ExampleCountMovieRated.py
@@ -2,11 +2,14 @@
 
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+
 class RatingsBreakdown (MRJob):
       def steps(self):
           return[
+                # multi MR jobs
                 MRStep(mapper = self.mapper_get_ratings,
-                reducer = self.reducer_count_ratings)
+                reducer = self.reducer_count_ratings),
+                MRStep(reducer = self.reducer_sorted_output)
           ]
 
        #Mapper function
@@ -14,9 +17,15 @@ class RatingsBreakdown (MRJob):
            (userID, movieID, rating, timestamp) = line.split('\t') 
             yield movieID,1
 
-        #Reducer function
-        def reducer_count_ratings(self,key,values):
+       #Reducer function
+       def reducer_count_ratings(self,key,values):
             yield key, sum(values)
+            
+       #Sort function
+      def reducer_sorted_output(self, count,movies):
+            for movie in movies:
+                  yield movie,count
+      
 
 if __name__ == '__main__':
 RatingBreakdown.run()

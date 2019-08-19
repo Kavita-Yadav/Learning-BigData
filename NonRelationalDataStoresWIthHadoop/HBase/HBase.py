43baf4bdae0a2ba0208bd1b1b1c9c41f1b79854c
Kavita Yadav <53824088+Kavita-Yadav@users.noreply.github.com>
2019-08-19 18:23:01 +0800

    Create HBase.py

---
+++ NonRelationalDataStoresWIthHadoop/HBase/HBase.py
@@ -0,0 +1,36 @@
+                                                                                                       Modified  
+
+from starbase import Connection
+
+c = Connection("127.0.0.1", "8000")
+
+ratings = c.table('ratings')
+
+if (ratings.exists()):
+        print("Dropping existing ratings table\n")
+        ratings.drop()
+
+ratings.create('rating')
+
+print("Parsing the ml-100l ratings data...\n")
+ratingFile = open("u.data", "r")
+
+batch = ratings.batch()
+
+for line in ratingFile:
+        (userID, movieID, rating, timestamp) = line.split()
+        batch.update(userID, {'rating':{movieID: rating}})
+
+ratingFile.close()
+
+print("Committing ratings data to HBase via REST service\n")
+batch.commit(finalize= True)
+
+print("Get back ratings for some users...\n")
+print("Ratings for user ID 1:\n")
+print(ratings.fetch("1"))
+print("Ratings for user ID 2:\n")
+print(ratings.fetch("33"))
+
+
+

f35fbd6a949f141a95f8ac1c3cb0bc98babc8fc3
Kavita Yadav <53824088+Kavita-Yadav@users.noreply.github.com>
2020-02-10 23:01:48 +0800

    Update MRJobExample.py

--- 1. HDFS-and-MapReduce/MRJobExample.py
+++ 1. HDFS-and-MapReduce/MRJobExample.py
@@ -1,20 +1,23 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 class RatingsBreakdown (MRJob):
+      # To define multiple steps, override steps() to return a list of MRSteps.
       def steps(self):
           return[
                 MRStep(mapper = self.mapper_get_ratings,
                 reducer = self.reducer_count_ratings)
           ]
 
-       #Mapper function
+       #Mapper function syntax: def mapper(self, _ , line)
        def mapper_get_ratings(self, _ , line):
-           (userID, movieID, rating, timestamp) = line.split('\t') 
+           (userID, movieID, rating, timestamp) = line.split('\t')
+            # yield sends a value back to caller
             yield rating,1
 
-        #Reducer function
+        #Reducer function syntax: def reducer(self, key, values)
         def reducer_count_ratings(self,key,values):
             yield key, sum(values)
 
 if __name__ == '__main__':
+   # running main class
    RatingBreakdown.run()
